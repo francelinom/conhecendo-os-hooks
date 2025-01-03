b881faf4dfdcc2ec7972fd803f11c1855cc7c6d7
Francelino <francelino.silva.prestserv@petrobras.com.br>
2025-01-03 13:10:09 -0300

    Feat: utilizando o () => para chamar uma função apenas uma vez

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
 
+function getInitialValue() {
+    return 1 + 1;
+}
+
 export default function useCounter() {
-  const [count, setCount] = useState(0);
+    /**
+     * () => getInitialValue() é uma função que retorna o valor inicial do estado
+     * E que deixa otimizado, fazendo apenas uma chamada da função getInitialValue
+     * Se não adicionar o " () => " toda vez que chamar a função ela vai está executando sempre
+     * então se for uma função complexa vai causar um impacto na performance
+     * Porque a função so serve para pegar o valor inicial!
+     */
+  const [count, setCount] = useState(() => getInitialValue());
 
   const increment = () => {
     setCount((count) => count + 1);
